f3e70fcdfdf5735670e2d73c002416d252ac115f
jonayeds <jonayeeds@gmail.com>
2025-03-11 13:52:04 +0600

    added index route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, { Application, Request, Response } from "express"
 import cors from "cors"
-import { UserRoutes } from "./app/modules/user/user.routes"
 import { errorHandler } from "./app/middlewares/globalErrorHandler"
 import { notFound } from "./app/middlewares/notFound"
+import router from "./app/routes"
 const app:Application = express()
 
 // persers
@@ -12,7 +12,7 @@ app.use(cors())
 
 
 // application Routes
-app.use("/api/v1/user", UserRoutes)
+app.use("/api/v1/", router)
 
 
 app.get("/", (req:Request,res:Response)=>{
---
+++ src/app/routes/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { UserRoutes } from "../modules/user/user.routes"
+
+const router = express.Router()
+
+const moduleRoutes = [
+    {
+        path:"/user",
+        routes:UserRoutes
+    }
+]
+
+moduleRoutes.forEach(route=> router.use(route.path, route.routes))
+
+export default router
